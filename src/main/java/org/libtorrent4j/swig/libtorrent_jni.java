d832d44e747de46be51a78e1dbceb7ca0d50b816
Alden Torres <aldenml@gmail.com>
2018-11-09 07:57:37 -0500

    using libtorrent master HEAD, version 1.2.0.20-RC1 (#10)

--- src/main/java/org/libtorrent4j/swig/libtorrent_jni.java
+++ src/main/java/org/libtorrent4j/swig/libtorrent_jni.java
@@ -13,7 +13,7 @@ public class libtorrent_jni {
 
     public static String libtorrent4jVersion() {
         // extracted from the gradle with the run-swig step
-        return "1.2.0.19";
+        return "1.2.0.20-RC1";
     }
 
     static {
