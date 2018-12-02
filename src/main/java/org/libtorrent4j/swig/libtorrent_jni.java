2d99dfb903f45c4b1ecb8eae81ab291eedfb443c
Alden Torres <aldenml@gmail.com>
2018-12-02 14:19:28 -0500

    version 1.2.0.20-RC3 (#22)

--- src/main/java/org/libtorrent4j/swig/libtorrent_jni.java
+++ src/main/java/org/libtorrent4j/swig/libtorrent_jni.java
@@ -13,7 +13,7 @@ public class libtorrent_jni {
 
     public static String libtorrent4jVersion() {
         // extracted from the gradle with the run-swig step
-        return "1.2.0.20-RC2";
+        return "1.2.0.20-RC3";
     }
 
     static {
