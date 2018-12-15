f02281c6d2954cadedc21a2aa5737025ab8bc395
Alden Torres <aldenml@gmail.com>
2018-12-15 11:09:12 -0500

    version 1.2.0.21-alpha1

--- src/main/java/org/libtorrent4j/swig/libtorrent_jni.java
+++ src/main/java/org/libtorrent4j/swig/libtorrent_jni.java
@@ -13,7 +13,7 @@ public class libtorrent_jni {
 
     public static String libtorrent4jVersion() {
         // extracted from the gradle with the run-swig step
-        return "1.2.0.21-RC1";
+        return "1.2.0.21-alpha1";
     }
 
     static {
