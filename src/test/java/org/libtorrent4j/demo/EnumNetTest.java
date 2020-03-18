fb0e87fae23533f7a0a9f66c594df96d1e3ba005
Alden Torres <aldenml@gmail.com>
2020-03-18 15:17:16 -0400

    restored EnumNet features (#66)

--- src/test/java/org/libtorrent4j/demo/EnumNetTest.java
+++ src/test/java/org/libtorrent4j/demo/EnumNetTest.java
@@ -1,56 +1,31 @@
 package org.libtorrent4j.demo;
 
-import org.libtorrent4j.AlertListener;
 import org.libtorrent4j.EnumNet;
-import org.libtorrent4j.SessionManager;
-import org.libtorrent4j.alerts.Alert;
+import org.libtorrent4j.swig.session;
 
 import java.util.List;
 
 /**
- * @author gubatron
  * @author aldenml
  */
 public final class EnumNetTest {
 
-    /*
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) {
+        session s = new session();
 
-        SessionManager s = new SessionManager();
-
-        s.addListener(new AlertListener() {
-            @Override
-            public int[] types() {
-                return null;
-            }
-
-            @Override
-            public void alert(Alert<?> alert) {
-                //System.out.println(alert);
-            }
-        });
-
-        s.start();
+        System.out.println("Enum routers");
+        List<EnumNet.IpRoute> routes = EnumNet.enumRoutes(s);
+        for (EnumNet.IpRoute route : routes) {
+            System.out.println(route);
+        }
 
         System.out.println("Enum interfaces");
         List<EnumNet.IpInterface> ipInterfaces = EnumNet.enumInterfaces(s);
         for (EnumNet.IpInterface iface : ipInterfaces) {
             System.out.println(iface);
+            System.out.println("Gateway: " + EnumNet.getGateway(iface, routes));
         }
-
-        System.out.println("Enum routers");
-        List<EnumNet.IpRoute> ipRoutes = EnumNet.enumRoutes(s);
-        for (EnumNet.IpRoute route : ipRoutes) {
-            System.out.println(route);
-        }
-
-        System.out.println("Default gateway: " + EnumNet.defaultGateway(s, "", false));
 
         System.out.println("Press ENTER to exit");
-        System.in.read();
-
-        s.stop();
     }
-
-     */
 }
