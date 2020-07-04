242f3807a7e51478f2dbe0f39dae70449cda25c9
Alden Torres <aldenml@gmail.com>
2020-07-04 17:08:00 -0400

    building with webtorrent on

--- swig/build-macos.sh
+++ swig/build-macos.sh
@@ -4,6 +4,10 @@ export BOOST_ROOT=$DEVELOPMENT_ROOT/boost_1_73_0
 export LIBTORRENT_ROOT=$DEVELOPMENT_ROOT/libtorrent
 export OPENSSL_ROOT=$DEVELOPMENT_ROOT/openssl-macos
 
+# minor fix to libtorrent build
+rm -rf $LIBTORRENT_ROOT/deps/libdatachannel/deps/libjuice/build
+rm -rf $LIBTORRENT_ROOT/deps/libdatachannel/deps/usrsctp/build
+
 $BOOST_ROOT/b2 -j8 --user-config=config/macos-x86_64-config.jam variant=release toolset=darwin-x86_64 target-os=darwin location=bin/release/macos/x86_64
 dsymutil bin/release/macos/x86_64/libtorrent4j.dylib -o bin/release/macos/x86_64/libtorrent4j.dSYM
 strip -x bin/release/macos/x86_64/libtorrent4j.dylib
