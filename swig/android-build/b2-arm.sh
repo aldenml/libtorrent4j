69723bd8466810f42389f7458292fcc4d3aa6b59
Alden Torres <aldenml@gmail.com>
2021-03-13 14:58:21 -0500

    using android NDK r21e, prebuilt toolchain and LLD linker (#168)

--- swig/android-build/b2-arm.sh
+++ swig/android-build/b2-arm.sh
@@ -3,16 +3,19 @@
 rm -rf /libtorrent4j/swig/deps/libtorrent/deps/libdatachannel/deps/libjuice/build-*
 rm -rf /libtorrent4j/swig/deps/libtorrent/deps/libdatachannel/deps/usrsctp/build-*
 
-alias ranlib='/android-toolchain-arm/bin/arm-linux-androideabi-ranlib'
-export ANDROID_TOOLCHAIN=/android-toolchain-arm
-export PATH=${ANDROID_TOOLCHAIN}/arm-linux-androideabi/bin:${PATH}
+alias ranlib='/android-ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib'
+export ANDROID_TOOLCHAIN=/android-ndk/toolchains/llvm/prebuilt/linux-x86_64
 export BOOST_ROOT=/boost
 export OPENSSL_ROOT=/openssl-arm
 export LIBTORRENT_ROOT=/libtorrent4j/swig/deps/libtorrent
-export CXX=${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-clang++
-export CC=${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-clang
+export CXX=${ANDROID_TOOLCHAIN}/bin/armv7a-linux-androideabi19-clang++
+export CC=${ANDROID_TOOLCHAIN}/bin/armv7a-linux-androideabi19-clang
+export AR=${ANDROID_TOOLCHAIN}/bin/llvm-ar
+export LD=${ANDROID_TOOLCHAIN}/bin/ld
+export RANLIB=${ANDROID_TOOLCHAIN}/bin/llvm-ranlib
+
 cd /libtorrent4j/swig
 ${BOOST_ROOT}/b2 -j2 --user-config=config/android-arm-config.jam variant=release toolset=clang-linux-arm target-os=android location=bin/release/android/armeabi-v7a
-${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-objcopy --only-keep-debug bin/release/android/armeabi-v7a/libtorrent4j.so bin/release/android/armeabi-v7a/libtorrent4j.so.debug
-${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-strip --strip-unneeded -x -g bin/release/android/armeabi-v7a/libtorrent4j.so
-${ANDROID_TOOLCHAIN}/bin/arm-linux-androideabi-readelf -d bin/release/android/armeabi-v7a/libtorrent4j.so
+${ANDROID_TOOLCHAIN}/bin/llvm-objcopy --only-keep-debug bin/release/android/armeabi-v7a/libtorrent4j.so bin/release/android/armeabi-v7a/libtorrent4j.so.debug
+${ANDROID_TOOLCHAIN}/bin/llvm-strip --strip-unneeded -x -g bin/release/android/armeabi-v7a/libtorrent4j.so
+${ANDROID_TOOLCHAIN}/bin/llvm-readelf -d bin/release/android/armeabi-v7a/libtorrent4j.so
--- swig/android-build/b2-x86.sh
+++ swig/android-build/b2-x86.sh
@@ -3,16 +3,19 @@
 rm -rf /libtorrent4j/swig/deps/libtorrent/deps/libdatachannel/deps/libjuice/build-*
 rm -rf /libtorrent4j/swig/deps/libtorrent/deps/libdatachannel/deps/usrsctp/build-*
 
-alias ranlib='/android-toolchain-x86/bin/i686-linux-android-ranlib'
-export ANDROID_TOOLCHAIN=/android-toolchain-x86
-export PATH=${ANDROID_TOOLCHAIN}/i686-linux-android/bin:${PATH}
+alias ranlib='/android-ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib'
+export ANDROID_TOOLCHAIN=/android-ndk/toolchains/llvm/prebuilt/linux-x86_64
 export BOOST_ROOT=/boost
 export OPENSSL_ROOT=/openssl-x86
 export LIBTORRENT_ROOT=/libtorrent4j/swig/deps/libtorrent
-export CXX=${ANDROID_TOOLCHAIN}/bin/i686-linux-android-clang++
-export CC=${ANDROID_TOOLCHAIN}/bin/i686-linux-android-clang
+export CXX=${ANDROID_TOOLCHAIN}/bin/i686-linux-android19-clang++
+export CC=${ANDROID_TOOLCHAIN}/bin/i686-linux-android19-clang
+export AR=${ANDROID_TOOLCHAIN}/bin/llvm-ar
+export LD=${ANDROID_TOOLCHAIN}/bin/ld
+export RANLIB=${ANDROID_TOOLCHAIN}/bin/llvm-ranlib
+
 cd /libtorrent4j/swig
 ${BOOST_ROOT}/b2 -j2 --user-config=config/android-x86-config.jam variant=release toolset=clang-x86 target-os=android location=bin/release/android/x86
-${ANDROID_TOOLCHAIN}/bin/i686-linux-android-objcopy --only-keep-debug bin/release/android/x86/libtorrent4j.so bin/release/android/x86/libtorrent4j.so.debug
-${ANDROID_TOOLCHAIN}/bin/i686-linux-android-strip --strip-unneeded -x -g bin/release/android/x86/libtorrent4j.so
-${ANDROID_TOOLCHAIN}/bin/i686-linux-android-readelf -d bin/release/android/x86/libtorrent4j.so
+${ANDROID_TOOLCHAIN}/bin/llvm-objcopy --only-keep-debug bin/release/android/x86/libtorrent4j.so bin/release/android/x86/libtorrent4j.so.debug
+${ANDROID_TOOLCHAIN}/bin/llvm-strip --strip-unneeded -x -g bin/release/android/x86/libtorrent4j.so
+${ANDROID_TOOLCHAIN}/bin/llvm-readelf -d bin/release/android/x86/libtorrent4j.so
