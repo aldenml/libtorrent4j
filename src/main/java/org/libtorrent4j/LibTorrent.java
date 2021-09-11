a746ce2bec16ec761334ff981a57669b3a269809
Alden Torres <aldenml@gmail.com>
2021-09-11 14:58:41 -0400

    fix for new network access restrictions in android 11 (#198)

--- src/main/java/org/libtorrent4j/LibTorrent.java
+++ src/main/java/org/libtorrent4j/LibTorrent.java
@@ -71,7 +71,7 @@ public final class LibTorrent {
      * @return libtorrent4j version.
      */
     public static String libtorrent4jVersion() {
-        return "2.0.4-21";
+        return "2.0.4-22-RC1";
     }
 
     /**
