0655b893a85ace222c2041b8a444a0bca544d816
Alden Torres <aldenml@gmail.com>
2021-12-18 18:36:07 -0500

    fix

--- src/test/java/org/libtorrent4j/SettingsPackTest.java
+++ src/test/java/org/libtorrent4j/SettingsPackTest.java
@@ -28,7 +28,7 @@ public class SettingsPackTest {
 
         String value = new String(fingerprint, 0, 8, StandardCharsets.US_ASCII);
 
-        assertEquals("-LT2040-", value);
+        assertEquals("-LT2050-", value);
     }
 
     @Test
