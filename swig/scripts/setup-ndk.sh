5066f501486b27dd1e67a9c7d7440872a0fabb9a
Alden Torres <aldenml@gmail.com>
2022-01-14 23:19:22 -0500

    update openssl to 1.1.1m and android ndk to r23b (#208)

--- swig/scripts/setup-ndk.sh
+++ swig/scripts/setup-ndk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -nv -O android-ndk.zip https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux-x86_64.zip
+wget -nv -O android-ndk.zip https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux.zip
 unzip -qq android-ndk.zip
 mv android-ndk-${NDK_VERSION} android-ndk
