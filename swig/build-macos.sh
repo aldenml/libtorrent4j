f2330ae18b646b1d54ee3f8573246ab9a2790548
Alden Torres <aldenml@gmail.com>
2025-07-19 14:48:43 -0400

    update to boost 1.88.0

--- swig/build-macos.sh
+++ swig/build-macos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export BOOST_ROOT=$DEVELOPMENT_ROOT/boost_1_86_0
+export BOOST_ROOT=$DEVELOPMENT_ROOT/boost_1_88_0
 export LIBTORRENT_ROOT=$PWD/deps/libtorrent
 export OPENSSL_ROOT=$DEVELOPMENT_ROOT/openssl-macos
 
