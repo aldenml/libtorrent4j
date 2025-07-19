e73b1a397a4b24fa4b99d376178888c804032450
Alden Torres <aldenml@gmail.com>
2025-07-19 17:45:30 -0400

    version 2.1.0-36 (#296)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2024, Alden Torres
+ * Copyright (c) 2018-2025, Alden Torres
  *
  * Licensed under the terms of the MIT license.
  * Copy of the license at https://opensource.org/licenses/MIT
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "org.libtorrent4j"
-version = "2.1.0-35"
+version = "2.1.0-36"
 
 java {
     // using java 8 for android compatibility
