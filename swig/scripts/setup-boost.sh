c7f2949adfc92966cccc738514d7b9e7971213c0
Alden Torres <aldenml@gmail.com>
2025-07-18 23:21:28 -0400

    fix

--- swig/scripts/setup-boost.sh
+++ swig/scripts/setup-boost.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -nv -O boost.tar.gz https://archives.boost.io/artifactory/main/release/${BOOST_DOT_VERSION}/source/boost_${BOOST_VERSION}.tar.gz
+wget -nv -O boost.tar.gz https://archives.boost.io/release/${BOOST_DOT_VERSION}/source/boost_${BOOST_VERSION}.tar.gz
 tar xzf boost.tar.gz
 mv boost_${BOOST_VERSION} boost-src
 pushd boost-src
